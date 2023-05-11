947253fc2fc90bf162170df61d9e4699c4e05748
Prayag Krishna <prayagkrishna2001@gmail.com>
2023-05-12 01:09:39 +0530

    automatically hits enter everytime

--- setup_everything.sh
+++ setup_everything.sh
@@ -8,9 +8,9 @@ timedatectl set-local-rtc 1 --adjust-system-clock
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer  
+echo -ne '\n' | sudo add-apt-repository ppa:danielrichter2007/grub-customizer  
 sudo apt-get update
-sudo apt upgrade -y
+sudo apt upgrade -yecho -ne '\n' | 
 sudo apt-get install grub-customizer -y
 
 sudo apt install gnome-shell-extension-manager -y
@@ -21,7 +21,7 @@ sudo apt install git -y
 
 sudo apt-get install flex bison -y
 
-sudo add-apt-repository ppa:linrunner/tlp
+echo -ne '\n' | sudo add-apt-repository ppa:linrunner/tlp
 sudo apt-get update
 sudo apt-get install tlp tlp-rdw
 systemctl enable tlp.service	
@@ -31,22 +31,22 @@ sudo snap install microsoft-todo-unofficial vlc
 
 sudo apt install software-properties-common apt-transport-https wget -y
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
+echo -ne '\n' | sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
 sudo apt install microsoft-edge-stable
 
 sudo apt-get install emacs -y
 
 sudo apt install build-essential -y
 
-sudo add-apt-repository ppa:apt-fast/stable
+echo -ne '\n' | sudo add-apt-repository ppa:apt-fast/stable
 sudo apt-get update
 sudo apt-get -y install apt-fast
 
-sudo apt-get install python3-pip
+sudo apt-get install python3-pip -y
 
 sudo snap install cmake --classic
 
-sudo add-apt-repository universe
+echo -ne '\n' | sudo add-apt-repository universe
 sudo apt update -y
 sudo apt install gnome-tweaks -y
 
